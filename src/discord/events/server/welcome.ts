8c802d27b946cde80eb5c221737548a8ef03bff1
Th4Hawk <whitereaper7777@protonmail.com>
2024-09-17 20:43:49 -0300

    First Commit

---
+++ src/discord/events/server/welcome.ts
@@ -0,0 +1,22 @@
+import { Event } from "#base";
+import { EmbedBuilder } from "discord.js";
+
+new Event({
+    name: "Welcome Message",
+    event: "guildMemberAdd",
+    async run(member) {
+        const embed = new EmbedBuilder()
+        .setTitle(`Seja bem vindo ${member.user.displayName}!`)
+        .setDescription("Esperamos que aproveite sua experiência no servidor!")
+        .setAuthor({
+            name: member.user.displayName,
+            iconURL: member.user.avatarURL() || undefined
+        })
+        .setImage("https://images-ext-1.discordapp.net/external/3kPAFHcrE-prN4GH-g-YOkr29wu4EgAJYvtJst7T4DY/%3Fsize%3D2048/https/cdn.discordapp.com/banners/1281421933413404703/93c1c50ac1cfa18045b1bafb54b755f0.png?format=webp&quality=lossless&width=400&height=225")
+        
+        const canal: any = member.guild.channels.cache.get("1284971617037979651");
+        
+        await canal.send({ embeds: [embed] });
+        
+    },
+})
